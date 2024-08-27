97d3fb2b17101eaf9c86da699b77beb60718304f
Vaibhav Pawar <vaibhavp@testrig.co.in>
2024-08-27 09:22:35 +0530

    updated folder structure and paths

--- recorder/vite.recorder.config.ts
+++ recorder/vite.recorder.config.ts
@@ -1,21 +1,5 @@
-/**
- * Copyright (c) Rui Figueira.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import path from 'path';
-import recorderConfig from '../../playwright/packages/recorder/vite.config';
+import recorderConfig from '../playwright/packages/recorder/vite.config';
 import type { UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import assert from 'assert';
@@ -34,7 +18,7 @@ export default defineConfig({
         handler: (html) => {
           // inject contentscript.ts in the recorder
           const result = html.replace(`<script type="module" src="/src/index.tsx"></script>`, `
-          <script type="module" src="../../../app/recorder/src/contentscript.ts"></script>
+          <script type="module" src="../../../recorder/src/contentscript.ts"></script>
           <script type="module" src="/src/index.tsx"></script>
           `);
           assert(html !== result, 'html should have been changed');
@@ -44,7 +28,7 @@ export default defineConfig({
     }
   ],
   base: './',
-  root: '../../playwright/packages/recorder',
+  root: '../playwright/packages/recorder',
   build: {
     ...userRecorderConfig.build,
     emptyOutDir: false,
